f343c5cc11a1230ff7680e4d643a59019326813b
Shubhankar Das <das.shubhankar97@gmail.com>
2025-03-05 14:14:00 +0530

    changes

--- main.py
+++ main.py
@@ -71,18 +71,17 @@ def train_lora(request: TrainRequest):
     output_dir = os.path.join(OUTPUTS_DIR, request.output_name)
     os.makedirs(output_dir, exist_ok=True)
 
-    command = f"""
-    accelerate launch --mixed_precision bf16 --num_cpu_threads_per_process 1 sd_scripts/flux_train_network.py 
-    --pretrained_model_name_or_path {MODELS_DIR}/{request.pretrained_model} --clip_l {MODELS_DIR}/{request.clip_l} --t5xxl {MODELS_DIR}/{request.t5xxl} 
-    --ae {request.ae} --cache_latents_to_disk --save_model_as safetensors --sdpa --persistent_data_loader_workers 
-    --max_data_loader_n_workers 2 --seed 42 --gradient_checkpointing --mixed_precision bf16 --save_precision bf16 
-    --network_module networks.lora_flux --network_dim {request.network_dim} --network_train_unet_only 
-    --optimizer_type adamw8bit --learning_rate {request.learning_rate} 
-    --cache_text_encoder_outputs --cache_text_encoder_outputs_to_disk --fp8_base 
-    --highvram --max_train_epochs {request.max_train_epochs} --save_every_n_epochs {request.save_every_n_epochs} --dataset_config {DATASETS_DIR}/{request.output_name}.toml 
-    --output_dir {output_dir} --output_name {request.output_name} 
-    --timestep_sampling shift --discrete_flow_shift 3.1582 --model_prediction_type raw --guidance_scale 1.0 --loss_type l2 {"--enable_bucket" if request.enable_bucket else ""} {"--full_bf16" if request.full_bf16 else ""}
-    """
+    command = f"""accelerate launch --mixed_precision bf16 --num_cpu_threads_per_process 1 sd_scripts/flux_train_network.py \
+--pretrained_model_name_or_path {MODELS_DIR}/{request.pretrained_model} --clip_l {MODELS_DIR}/{request.clip_l} --t5xxl {MODELS_DIR}/{request.t5xxl} \
+--ae {request.ae} --cache_latents_to_disk --save_model_as safetensors --sdpa --persistent_data_loader_workers \
+--max_data_loader_n_workers 2 --seed 42 --gradient_checkpointing --mixed_precision bf16 --save_precision bf16 \
+--network_module networks.lora_flux --network_dim {request.network_dim} --network_train_unet_only \
+--optimizer_type adamw8bit --learning_rate {request.learning_rate} \
+--cache_text_encoder_outputs --cache_text_encoder_outputs_to_disk --fp8_base \
+--highvram --max_train_epochs {request.max_train_epochs} --save_every_n_epochs {request.save_every_n_epochs} --dataset_config {DATASETS_DIR}/{request.output_name}.toml \
+--output_dir {output_dir} --output_name {request.output_name} \
+--timestep_sampling shift --discrete_flow_shift 3.1582 --model_prediction_type raw --guidance_scale 1.0 --loss_type l2 {"--enable_bucket" if request.enable_bucket else ""} {"--full_bf16" if request.full_bf16 else ""}
+"""
 
     try:
         print("Running command")
